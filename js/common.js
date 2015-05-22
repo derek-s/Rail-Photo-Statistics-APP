339dba3bef87005a6a1db1ac9b38200112682908
Derek.S_大D <bugsonglinux@gmail.com>
2015-05-22 23:47:12 +0800

    master
    Bata 2

--- js/common.js
+++ js/common.js
@@ -13,15 +13,16 @@ function addproc() {
 	var db_Location = sessionStorage.getItem('s_Location');
 	var db_Data = sessionStorage.getItem('s_Data');
 	var db_Locomodel = sessionStorage.getItem('s_Locomodel');
+	var db_Remark = sessionStorage.getItem('s_Remark');
 	var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
-	var def = 'id integer primary key AutoIncrement,Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel';
+	var def = 'id integer primary key AutoIncrement,Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel,Remark';
 	db.transaction(function(tx) {
 		tx.executeSql('CREATE TABLE IF NOT EXISTS Rail_locomotive (' + def + ')');
-		tx.executeSql('INSERT INTO Rail_locomotive (Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [db_Model, db_UIC, db_Power, db_Drive, db_Attach, db_Factory, db_Year, db_Type, db_Number, db_Section, db_Ub, db_Location, db_Data, db_Locomodel]);
+		tx.executeSql('INSERT INTO Rail_locomotive (Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel,Remark) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [db_Model, db_UIC, db_Power, db_Drive, db_Attach, db_Factory, db_Year, db_Type, db_Number, db_Section, db_Ub, db_Location, db_Data, db_Locomodel,db_Remark]);
 		alert("添加完成");
+		plus.webview.getWebviewById("main.html").reload(true);
 	});
 }
-
 function sequence() {
 	var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
 	db.transaction(function(tx) {
@@ -29,103 +30,153 @@ function sequence() {
 			var len = results.rows.length;
 			document.querySelector('#rows').innerHTML += len;
 			localStorage.setItem('zentry', results.rows.length);
-			console.log(len);
 		});
 	});
 }
-
 function backup() {
-	var csvdata = 'Model,Locomodel,Type,UIC,Power,Drive,Attach,Factory,Year,Number,Section,Ub,Location,Data\n';
+	var btnArray = ['是', '否'];
+		mui.confirm('确认备份数据么？', '拍车统计', btnArray, function(e) {
+			if (e.index == 0) {
 	var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
-	db.transaction(function(tx) {
-		db.transaction(function(tx) {
-			tx.executeSql('SELECT * FROM Rail_locomotive', [], function(tx, results) {
-				var len = results.rows.length,
-					i;
-				for (i = 0; i < len; i++) {
-					csvdata += results.rows.item(i).Model + "," + results.rows.item(i).Locomodel + "," + results.rows.item(i).Type + "," + results.rows.item(i).UIC + "," + results.rows.item(i).Power + "," + results.rows.item(i).Driver + "," + results.rows.item(i).Attach + "," + results.rows.item(i).Factory + "," + results.rows.item(i).Year + "," + results.rows.item(i).Number + "," + results.rows.item(i).Section + "," + results.rows.item(i).Ub + "," + results.rows.item(i).Location + "," + results.rows.item(i).Data + "\n"
-				};
-
-				console.log(csvdata);
+				db.transaction(function(tx) {
+		tx.executeSql('SELECT * FROM Rail_locomotive', [], function(tx, results) {
+						var data = convertResults(results);
 			});
-		});
-		plus.io.requestFileSystem(plus.io.PUBLIC_DOCUMENTS, function(fs) {
-			fs.root.getFile('backup.csv', {
-				create: true
-			}, function(fileEntry) {
-				console.log(fileEntry.fullPath);
-				fileEntry.createWriter(function(writer) {
-					writer.onwritestart = function(e) {
-						console.log('start');
-					};
-					writer.write(csvdata);
-				});
-			}, function(e) {
-				console.log(e.message);
-			});
-		}, function(e) {
-			console.log(e.message);
-		})
 	});
 }
-
+			});
+}
+function convertResults(resultset) {
+	var results = [];
+	for(var i=0,len=resultset.rows.length;i<len;i++) {
+		row = resultset.rows.item(i);
+		var result = {};
+		for(var key in row) {
+			result[key] = row[key];
+		}
+		results.push(result);
+	}
+	var serializedData = JSON.stringify(results);
+	mui.plusReady(function() {
+					plus.io.requestFileSystem(plus.io.PUBLIC_DOCUMENTS, function(fs) {
+						fs.root.getFile('backup.json', {
+							create: true
+						}, function(fileEntry) {
+							bfpath = fileEntry.fullPath;
+							fileEntry.createWriter(function(writer) {
+								writer.onwritestart = function(e) {
+									console.log('start')
+								};
+								writer.onprogress = function(e) {
+									console.log('progress')
+								};
+								writer.onwrite = function(e) {
+									console.log('onwrite')
+								};
+								writer.onabort = function(e) {
+									console.log('abort')
+								};
+								writer.onerror = function(e) {
+									console.log('error')
+								};
+								writer.onwriteend = function(e) {
+									console.log('writeend')
+								};
+								writer.onwritestart = function(e) {
+									console.log('writestart')
+								};
+								writer.write(serializedData);
+							},function(e){
+								console.log(e.message);
+							});
+							
+						},function(e){
+								console.log(e.message);
+							});
+					});
+					plus.nativeUI.alert('数据备份完成', function() {}, '拍车统计');
+					plus.webview.getWebviewById("setting.html").reload(true);
+				});		
+	return results;
+}
 function importbp() {
-		var reader = null;
-		plus.io.requestFileSystem(plus.io.PUBLIC_DOCUMENTS, function(fs) {
-			fs.root.getFile('backup.csv', {
-				create: true
-			}, function(fileEntry) {
-				console.log(fileEntry.fullPath);
-				fileEntry.file(function(file) {
-					var fileReader = new plus.io.FileReader();
-					fileReader.readAsText(file, 'utf-8');
-					fileReader.onloadend = function(evt) {
-						var csv = evt.target.result;
-						console.log(csv);
-						var bdata = csv.split("\n");
-						var bresult = [];
-						var bheaders = bdata[0].split(",");
-						for (var bi = 1; bi < bdata.length; bi++) {
-							var bobj = {};
-							var currrntline = bdata[bi].split(",");
-							for (var bj = 0; bj < bheaders.length; bj++) {
-								bobj[bheaders[bj]] = currrntline[bj];
-							}
-							bresult.push(bobj);
-						}
-						var jsondata = bresult;
-						jsondata.pop();
-						var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
-						var def = 'id integer primary key AutoIncrement,Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel';
-						db.transaction(function(tx) {
-							tx.executeSql('CREATE TABLE IF NOT EXISTS Rail_locomotive (' + def + ')');
-							for (var ji = 0; ji < jsondata.length; ji++) {
-								json_model = jsondata[ji].Model;
-								json_locomodel = jsondata[ji].Locomodel;
-								json_type = jsondata[ji].Type;
-								json_uic = jsondata[ji].UIC;
-								json_power = jsondata[ji].Power;
-								json_drive = jsondata[ji].Drive;
-								json_attach = jsondata[ji].Attach;
-								json_factory = jsondata[ji].Factory;
-								json_year = jsondata[ji].Year;
-								json_number = jsondata[ji].Number;
-								json_section = jsondata[ji].Section;
-								json_ub = jsondata[ji].Ub;
-								json_location = jsondata[ji].Location;
-								json_data = jsondata[ji].Data;
-								console.log(json_model);
-								tx.executeSql('INSERT INTO Rail_locomotive (Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [json_model, json_uic, json_power, json_drive, json_attach, json_factory, json_year, json_type, json_number, json_section, json_ub, json_location, json_data, json_locomodel]);
+	mui.plusReady(function() {
+		var btnArray = ['是', '否'];
+		mui.confirm('确认恢复数据么？', '拍车统计', btnArray, function(e) {
+			if (e.index == 0) {
+				var wait = plus.nativeUI.showWaiting("正在恢复，请稍后……");
+				var reader = null;
+				plus.io.requestFileSystem(plus.io.PUBLIC_DOCUMENTS, function(fs) {
+					fs.root.getFile('backup.json', {
+						create: true
+					}, function(fileEntry) {
+						console.log(fileEntry.fullPath);
+						fileEntry.file(function(file) {
+							var fileReader = new plus.io.FileReader();
+							fileReader.readAsText(file, 'utf-8');
+							fileReader.onloadend = function(evt) {
+								var csv = evt.target.result;
+								var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
+								var def = 'id integer primary key AutoIncrement,Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel,Remark';
+								var importdata = JSON.parse(csv);
+								db.transaction(function(tx) {
+									tx.executeSql('CREATE TABLE IF NOT EXISTS Rail_locomotive (' + def + ')');
+									for (o in importdata) {
+										json_model = importdata[o].Model;
+										json_locomodel = importdata[o].Locomodel;
+										json_type = importdata[o].Type;
+										json_uic = importdata[o].UIC;
+										json_power = importdata[o].Power;
+										json_drive = importdata[o].Drive;
+										json_attach = importdata[o].Attach;
+										json_factory = importdata[o].Factory;
+										json_year = importdata[o].Year;
+										json_number = importdata[o].Number;
+										json_section = importdata[o].Section;
+										json_ub = importdata[o].Ub;
+										json_location = importdata[o].Location;
+										json_data = importdata[o].Data;
+										json_Remark = importdata[o].Remark;
+										tx.executeSql('INSERT INTO Rail_locomotive (Model,UIC,Power,Drive,Attach,Factory,Year,Type,Number,Section,Ub,Location,Data,Locomodel,Remark) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [json_model, json_uic, json_power, json_drive, json_attach, json_factory, json_year, json_type, json_number, json_section, json_ub, json_location, json_data, json_locomodel,json_Remark]);
+									}
+									if (o == importdata.length-1) {
+										wait.close();
+										plus.nativeUI.alert('数据恢复完成', function() {}, '拍车统计');
+										plus.webview.getWebviewById("main.html").reload(true);
+										plus.webview.getWebviewById("setting.html").reload(true);
+									}
+								});
+
 							}
 						});
-					}
-				});
-			});
+					});
+
+				})
+			} else {}
 		})
-	}
+
+	})
+}
+
 function deltable() {
-	var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
-	db.transaction(function(tx) {
-		tx.executeSql('DROP TABLE Rail_locomotive');
+	var btnArray = ['是', '否'];
+	mui.confirm('确认删除数据么？', '拍车统计', btnArray, function(e) {
+		if (e.index == 0) {
+			var db = openDatabase('Rail', '1.0', 'RailAppDB', 10 * 1024 * 1024);
+			db.transaction(function(tx) {
+				tx.executeSql('DROP TABLE Rail_locomotive');
+			});
+			plus.nativeUI.alert('数据删除完毕', function() {}, '拍车统计');
+			plus.webview.getWebviewById("main.html").reload(true);
+
+		} else {}
+	})
+}
+
+function setting() {
+	mui('#topPopover').popover('hide');
+	mui.openWindow({
+		url: 'setting.html'
 	});
+
 }
